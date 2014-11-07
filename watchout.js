3a03c79b6e97a47a27eb183e1f928cc0472ce222
espiler <monkeysrawesome@gmail.com>
2014-11-07 15:02:14 -0800

    Draggable feature was added to player. Cursor is always an arrow when clicking player.

--- watchout.js
+++ watchout.js
@@ -7,9 +7,10 @@ var circles = new Array(30);
 
 
 var moveCircles = function(){
-  d3.selectAll('circle')
+  d3.selectAll('.enemy')
     .transition().duration(3000)
     .attr('cy',function(){ return Math.random()*window.innerHeight})
     .attr('cx',function(){ return Math.random()*window.innerWidth});
 };
+
 
