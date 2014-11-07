7c3b31b78609d74cf65f8d42d4517b954c1c730f
espiler <monkeysrawesome@gmail.com>
2014-11-07 14:35:57 -0800

    Enemies will now move to a random spot initial and every three seconds after the page is loaded.

--- watchout.js
+++ watchout.js
@@ -2,5 +2,14 @@
 //
 //
 
+var circles = new Array(30);
 
+
+
+var moveCircles = function(){
+  d3.selectAll('circle')
+    .transition().duration(3000)
+    .attr('cy',function(){ return Math.random()*window.innerHeight})
+    .attr('cx',function(){ return Math.random()*window.innerWidth});
+};
 
